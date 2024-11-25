12cd5474870093dd4fe7dcb3a423cb05391807d1
Chalita Chaiyachan <chalitachaiyachan@Chalitas-MacBook-Pro.local>
2024-11-25 20:30:58 +0100

    finiish the structure of quiz in html

---
+++ src/javascript/app.js
@@ -0,0 +1,40 @@
+const debug = true;
+const quizeQuestionsArray = [{
+    questions:"What position does Harry play on the Gryffindor Quidditch team?",
+    answers:["Seeker, 1", "Goalie, 0", "Defender,0"],
+},
+{
+    questions: "What kind of pets does Harry own?",
+    answers: ["Cat, 0", "Owl. 1", "A rat, 0"],
+}, 
+{
+    questions: "What is the name of the pub at the entrance of Diagon Alley?",
+    answers: ["The Leaky Cauldron, 1", "The Cauldron, 0", "The Faraway pub, 0"],
+},
+{
+    questions: "Who is the headmaster of Hogwarts when Harry arrives?", 
+    answers: ["Snape, 0", "Lockhart,0", "Dumbledore, 1"], 
+},
+{
+    questions: "Who does Harry live with before he went to Hogwarts?", 
+    answers: ["His parents,0", "His uncle, aunt and cousing,1", "his uncle and aunt,0"],
+},
+
+{
+    questions: "What is the Voldermorts full, real name?", 
+    answers: ["Timothy Riddle Marvelo,0", "Thomas Riddle Marvolo, 0", "Tom Marvolo Riddle,1"],
+},
+{
+    questions: "Who is Harrys nemesis throughout his time in Hogwarts?", 
+    answers: ["Draco Malfoy,1", "Ron Weasley,0", "Hermione Granger,0"],
+}, 
+{
+    questions: "In book 3, what is Neville Longbottoms greatest fear, revealed upon facing a boggart in Lupins class?",
+    answers: ["A clown, 0", "A snake, 0", "Professor Snape"],  
+},
+];
+
+if(debug){
+console.log(quizeQuestionsArray);
+}
+
