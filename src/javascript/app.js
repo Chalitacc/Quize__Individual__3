b3de1bb4cd3d62479caef11b439bd2306f722d90
Chalita Chaiyachan <chalitachaiyachan@Chalitas-MacBook-Pro.local>
2024-11-29 20:26:14 +0100

    finish quize javascript functionality

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,38 +1,105 @@
 const debug = true;
 const quizeQuestionsArray = [{
     questions:"What position does Harry play on the Gryffindor Quidditch team?",
-    answers:["Seeker, 1", "Goalie, 0", "Defender,0"],
+    answers:[{
+            text: "Seeker", isCorrect: true,
+        },
+        {
+            text: "Goalie", isCorrect: false,
+        },
+        {
+            text: "Defender", isCorrect: false,
+        }], 
 },
 {
     questions: "What kind of pets does Harry own?",
-    answers: ["Cat, 0", "Owl, 1", "A rat, 0"],
+    answers: [{
+        text: "Cat", isCorrect: false, 
+    },
+    {
+        text: "Owl", isCorrect: true,
+    }, 
+    {
+        text: "A rat", isCorrect:false,
+
+    }],
 }, 
 {
     questions: "What is the name of the pub at the entrance of Diagon Alley?",
-    answers: ["The Leaky Cauldron, 1", "The Cauldron, 0", "The Faraway pub, 0"],
+    answers: [{
+        text: "The Leaky Cauldron", isCorrect: true,
+    },
+    {
+        text: "The Cauldron", isCorrect: false,
+    },
+    {
+        text: "The Faraway pub", isCorrect: false,
+    }],
 },
 {
     questions: "Who is the headmaster of Hogwarts when Harry arrives?", 
-    answers: ["Snape, 0", "Lockhart,0", "Dumbledore, 1"], 
+    answers: [{
+        text: "Snape", isCorrect: false,
+    },
+    {
+        text: "Lockhart", isCorrect: false,
+    },
+    {
+        text: "Dumbledore", isCorrect: true,
+    }], 
 },
 {
     questions: "Who does Harry live with before he went to Hogwarts?", 
-    answers: ["His parents,0", "His uncle and aunt and cousing,1", "his uncle and aunt,0"],
+    answers: [{
+        text: "His parents", isCorrect: false,
+    },
+    {
+        text: "His uncle and aunt and cousing", isCorrect: true,
+    },
+    {
+        text: "his uncle and aunt", isCorrect: false,
+    }],
 },
 
 {
     questions: "What is the Voldermorts full, real name?", 
-    answers: ["Timothy Riddle Marvelo,0", "Thomas Riddle Marvolo, 0", "Tom Marvolo Riddle,1"],
+    answers: [{
+        text: "Timothy Riddle Marvelo", isCorrect: false,
+    },
+    {
+        text: "Thomas Riddle Marvolo", isCorrect: false,
+    },
+    {
+        text: "Tom Marvolo Riddle", isCorrect: true
+    }],
 },
 {
     questions: "Who is Harrys nemesis throughout his time in Hogwarts?", 
-    answers: ["Draco Malfoy,1", "Ron Weasley,0", "Hermione Granger,0"],
+    answers: [{
+        text: "Draco Malfoy", isCorrect: true,
+    },
+    {
+        text: "Ron Weasley", isCorrect: false,
+    },
+    {
+        text: "Hermione Granger", isCorrect: false
+    }],
 }, 
 {
     questions: "In book 3, what is Neville Longbottoms greatest fear, revealed upon facing a boggart in Lupins class?",
-    answers: ["A clown, 0", "A snake, 0", "Professor Snape, 1"],  
+    answers: [{
+        text: "Snape", isCorrect: false,
+    },
+    {
+        text: "A clown", isCorrect: false,
+    },
+    {
+        text: "Professor Snape", isCorrect: true,
+    }],  
 },
-];
+]
+
+let allAnswers = []
 
 if(debug){
 console.log(quizeQuestionsArray);
@@ -56,7 +123,6 @@ displayquestions(currentQuestionIndex);
 
 function displayquestions(elementNumber){
     const currentQuestion = quizeQuestionsArray[elementNumber];
-
     const questionElement = document.querySelector(".quize__questions")
     questionElement.textContent = currentQuestion.questions;
 
@@ -64,99 +130,167 @@ function displayquestions(elementNumber){
 
     const answerOptions = document.querySelector(".answer__options")
     answerOptions.innerHTML = ``;
-    console.log(answerOptions);
 
     //implementing radio and label button 
+
     currentQuestion.answers.forEach((answers, index)=>{
-        const [answersText, isCorrect] = answers.split(",");
+
+        console.log(answers, "HELLO")
+
+        const {text, isCorrect} = answers;
+
+        console.log(text, isCorrect, "HELLO2222");
 
         const radio = document.createElement("input");
         radio.type = "radio"; 
         radio.name = "answers";
-        radio.value = isCorrect.trim();
+
+        if (isCorrect) {
+            radio.value = 1
+        } else {
+            radio.value = 0
+        }
+        
         radio.id = `answers${index}`
 
        const label = document.createElement("label");
        label.htmlFor = radio.id;
-       label.textContent = answersText;
+       label.textContent = text;
 
-       answerOptions.appendChild(radio);
-       answerOptions.appendChild(label);
-       answerOptions.appendChild(document.createElement("br"));
+       const inputContainer = document.createElement("div")
+       
+
+       answerOptions.appendChild(inputContainer);
+
+       
+        inputContainer.appendChild(radio)
+        inputContainer.appendChild(label)
 
     });
 
                 //text button progression
-        buttonText.textContent = `${elementNumber+ 1} of ${quizeQuestionsArray.length}` 
+        buttonText.textContent = `${elementNumber+1} of ${quizeQuestionsArray.length}` 
 
     }
+
+     //trenger jeg denne???
+
         progressionButton.addEventListener("click", function(){
             nextQuestion();
         });
 
         //handling next question + tracking score
+
+      
         
         function nextQuestion(){
             const selectedAnswer = document.querySelector(`input[name="answers"]:checked`);
+            const selectedAnswerId = selectedAnswer.id
+            console.log(selectedAnswerId, "selectedAnswerId")
+            const selectedAnswerText = document.querySelector(`label[for="${selectedAnswerId}"]`)?.innerText
+            
+            if (!selectedAnswer){
+                alert("Please choose an answer");
+              return;
+             }
+             const scoreNumber = parseInt(selectedAnswer.value);
+             totalScore += scoreNumber; 
+            
+             allAnswers.push({answerText: selectedAnswerText, wasCorrect: scoreNumber ? true: false})
+
+             console.log(allAnswers, "answers")
+     
+             currentQuestionIndex++;
+           
+           if (currentQuestionIndex < quizeQuestionsArray.length){
+             displayquestions(currentQuestionIndex);
+           } else
+             displayScore();
         }
-        //     if (!selectedAnswer){
-        //     alert("Please choose an answer ⚡️");
-        //     return;
-        // }
-
-        //score tracking 
-
-      const scoreNumber = parseInt(selectedAnswer.value, 10);
-        totalScore += scoreNumber; 
-
-        console.log("You scored:", scoreNumber, "Total Score", totalScore);
-
-        currentQuestionIndex++;
       
-      if (currentQuestionIndex < quizeQuestionsArray.length){
-        displayquestions(currentQuestionIndex);
-      } else
-        displayScore();
 
 
-        
-    
-    
 
-
+   
     //function to go to the next question button
     progressionButton.addEventListener("click", function(){
-    currentquestion++;
-    if (currentquestion < quizeQuestionsArray.length) {
-        displayquestions(currentquestion);
+    if (currentQuestionIndex < quizeQuestionsArray.length) {
+        displayquestions(currentQuestionIndex);
     } else{
-        displayScore();
+       /*  displayScore(); */
+        console.log(allAnswers, "HEI HEI HEI HEIE HIEHFusdhfisdhfksdhfijksdhi")
     }
 }); 
 
 function displayScore(){
     console.log(`Quize is complete! Your score is: ${totalScore}`);
 
-    //redirecting user to another page so they can see their score
-    window.location.href = "score.html";
+    const scoreDisplay = document.querySelector(".quize__score")
+    const scoreHeader = document.createElement("h3")
+    scoreHeader.innerText = "Your Results:"
+
+    
+    scoreDisplay.appendChild(scoreHeader)
+    
+
+    quizeQuestionsArray.map((question, qIndex) => {
+        const box = document.createElement("div")
+        const text = document.createElement("p")
+
+   
+
+
+        box.style.flex
+        box.style.flexDirection = "column"
+        box.style.border = "2px solid black"
+        box.style.width = "200px"
+
+
+
+        text.innerText = question.questions
+        text.style.color = question.answers
+        
+        scoreDisplay.appendChild(box)
+        box.appendChild(text)
+
+
+        question.answers.forEach((answer, aIndex) => {
+            const answerBox = document.createElement("div")
+            const answerText = document.createElement("p")
+
+            answerBox.width = "100%"
+
+            const selectedAnswerColor = "orange"
+            const rightAnswer = answer.isCorrect
+
+            let selectedAnswerEmoji = ""
+
+            if (rightAnswer) {
+                selectedAnswerEmoji = "✅"
+            } else {
+                selectedAnswerEmoji = "❌"
+            }
+
+           if (allAnswers[qIndex].answerText === answer.text) {
+            answerBox.style.border = "2px solid" + selectedAnswerColor
+           } else {
+             answerBox.style.border = "none"
+           }
+
+            answerText.innerText = quizeQuestionsArray[qIndex].answers[aIndex].text + " " + selectedAnswerEmoji
+
+            box.appendChild(answerBox)
+            answerBox.appendChild(answerText)
+
+        })
+
+
+
+
+
+
+    })
+   
     
 }
 
-
-
-
-
-
-
-
-// const quizeQuestion = document.querySelector(".quize__questions");
-// quizeQuestion.textContent = quizeQuestionsArray[elementNumber].questions;   
-// console.log(renderQuize);
-
-// const quizeAnswersText = document.querySelector(".label__answers");
-// quizeAnswersText.textContent = quizeQuestionsArray[elementNumber].answers;
-// console.log(quizeAnswersText);
-
-
-// const quizeRadio = document.querySelector(".radio__buttons");
-// quizeRadio.radioButtons = quizeQuestionsArray[elementNumber].answers;
